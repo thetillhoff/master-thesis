70d6672945533408b4d6f752760f70f8261f78da
Till Hoffmann <till.hoffmann@thetillhoff.de>
2021-08-16 13:51:04 +0200

    improved validation cmd

--- src/cmd/validate.go
+++ src/cmd/validate.go
@@ -33,52 +33,38 @@ import (
 var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "Load CSAR and validate contents (== try to parse to TOSCA service template)",
-	Long: `Load CSAR from zip-archive (proper CSAR, therefore default) OR from a folder containing the extracted contents of a CSAR.
+	Long: `Load CSAR either from zip-archive (proper CSAR, detected by extension of provided path) OR from a folder containing the extracted contents of a CSAR.
 	Usage examples:
 
 	eat validate example-csar.zip
-	eat validate -d example-csar/
-	eat validate --directory example-csar/
+	eat validate example-csar/
 `,
-	Args: cobra.MaximumNArgs(1),
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		var (
-			csarPath string
+			csarPath string = args[0]
 			archive  csar.CSAR
 		)
 
 		if debug {
-			log.Println("debug:", debug)
 			// Set debug for imports
 			csar.Debug = debug
+
+			log.Println("INF debug:", debug)
+
+			log.Println("INF csarPath:", csarPath)
 		}
 
-		// Retrieve flag
-		extractedCSAR, err := cmd.Flags().GetString("directory")
-		if err != nil {
-			log.Fatalln("ERR There was an error while reading the flag 'directory':", err)
+		archive = csar.LoadFromPath(csarPath)
+		if debug {
+			log.Println("SUC Loaded CSAR from '" + csarPath + "'.")
 		}
 
-		// Validate usage and run CSAR validation
-		if len(args) == 1 && extractedCSAR == "" { // Load CSAR from zip-archive
-			log.Fatalln("// TODO: Implement LoadFromFile")
-			csarPath = args[0]
-			archive = csar.LoadFromFile(csarPath)
-			if debug {
-				log.Println("SUC Loaded CSAR from file at '" + csarPath + "'.")
-			}
-			csar.PrintCSAR(archive)
-		} else if len(args) == 0 && extractedCSAR != "" { // Load extracted CSAR from directory
-			archive = csar.LoadFromFolder(extractedCSAR)
-			if debug {
-				log.Println("SUC Loaded CSAR from folder at '" + extractedCSAR + "'.")
-			}
-			csar.PrintCSAR(archive)
-		} else if len(args) == 0 && extractedCSAR == "" {
-			log.Fatalln("ERR No CSAR source provided.")
-		} else {
-			log.Fatalln("ERR Concurrent loading of CSAR from zip-archive and from directory is not possible.")
-		}
+		// for _, x := range archive.ServiceTemplate.DataTypes {
+		// 	x.ValidateConstraints()
+		// }
+
+		csar.PrintCSAR(archive)
 	},
 }
 
@@ -95,5 +81,4 @@ func init() {
 	// is called directly, e.g.:
 	// validateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-	validateCmd.Flags().StringP("directory", "d", "", "Load extracted CSAR contents from directoy instead of zip-archive.")
 }
